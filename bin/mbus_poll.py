5f7001e1e25270d3b0c1483de88389a6bac415d0
sourceperl <loic.celine@free.fr>
2014-11-23 16:51:40 +0100

    update mbus_poll.py

--- bin/mbus_poll.py
+++ bin/mbus_poll.py
@@ -4,9 +4,10 @@ import rrdtool
 import time
 
 # some consts
-RRD_POS  = "/home/pi/rrd/pos.rrd"
-RRD_FLOW = "/home/pi/rrd/flow.rrd"
-RRD_SP   = "/home/pi/rrd/setpoint.rrd"
+RRD_REFRESH = 1.0 # refresh RRD every 1s
+RRD_POS     = "/home/pi/rrd/pos.rrd"
+RRD_FLOW    = "/home/pi/rrd/flow.rrd"
+RRD_SP      = "/home/pi/rrd/setpoint.rrd"
 
 c=ModbusClient()
 #c.debug(1)
@@ -14,8 +15,14 @@ c.host("163.111.184.31")
 c.unit_id(33)
 
 while(True):
-  if c.open():
-    # #1536
+  # keep TCP link open
+  if not c.is_open():
+    c.open()
+
+  if c.is_open():
+    # loop start time
+    start = time.time()
+    # #20506
     r = c.read_holding_registers(20506)
     if r:
       ret = rrdtool.update(RRD_POS, 'N:%d' % r[0])
@@ -27,5 +34,11 @@ while(True):
     r = c.read_holding_registers(20494)
     if r:
       ret = rrdtool.update(RRD_SP, 'N:%d' % r[0])
-    c.close()
-  time.sleep(1)
+    # loop end time
+    end = time.time()
+    loop_time = end - start
+    # wait before next cycle
+    time.sleep(RRD_REFRESH - loop_time)
+  else:
+    # wait fix 1s
+    time.sleep(1)
